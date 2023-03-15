4c5aa26cafbab3400cae92f7388077e374278b9b
Nicholas1501 <nicholasfarrelxp@gmail.com>
2023-03-15 01:11:54 -0300

    new problem

--- teleporters_hard.cpp
+++ teleporters_hard.cpp
@@ -20,11 +20,59 @@ struct teleport
     bool porzero;
 };
 
-bool compareByValue(const teleport &a, const teleport &b) {
-    return a.value < b.value;
+bool compareByValue(const teleport &tele_a, const teleport &tele_b) {
+    return tele_a.value < tele_b.value;
 }
 
-int first_appear(vector<teleport> tele, int low, int high) {
+int binary_search_leftmost(const vector<teleport>& tele_vector, int x) {
+    int low = 1;
+    int high = tele_vector.size() - 1;
+    int result = -1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (tele_vector[mid].value == x) {
+            result = mid;
+            high = mid - 1;
+        } else if (tele_vector[mid].value < x) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return result;
+}
+
+int valor_simetrico(const teleport &tele_obj, int n) {
+    int valorSimetrico;
+    int index = tele_obj.index;
+    if (tele_obj.porzero) {
+        valorSimetrico = tele_obj.value + n - 2 * index + 1;
+    }
+    else {
+        valorSimetrico = tele_obj.value + 2 * index - n - 1;
+    }
+    return valorSimetrico;
+}
+
+void remover_simetricos(vector<teleport>& tele_vector, teleport &tele_obj, int posicao, int n) {
+    int index = tele_obj.index;
+    int valorSimetrico = valor_simetrico(tele_obj, n);
+
+    //apaga o teletransporte tele_obj
+    tele_vector.erase(tele_vector.begin() + posicao);
+
+    //binary search to find the value to remove
+    int i = binary_search_leftmost(tele_vector, valorSimetrico);
+
+    // como podemos ter mais de um elemento no vetor com valor valueRemove
+    // pegamos o índice mais a esquerda e procuramos o valor certo
+    while(tele_vector[i].index != index) {
+        i++;
+    }
+
+    // agora, basta remover ele da lista também
+    tele_vector.erase(tele_vector.begin() + i);
 
 }
 
@@ -37,10 +85,10 @@ void solve() {
 
     //inicializando os objetos de teletransporte
     int cur;
-    for (int i = 1; i < n + 1; i++) {
+    for (int i = 1; i <= n; i++) {
         cin >> cur;
         tele[i] = {cur + i, i, true};
-        tele[i + 1] = {cur + n - i + 1, i, false};
+        tele[n + i] = {cur + n - i + 1, i, false};
     }
 
     //ordenar objetos de teletransporte por valor
@@ -48,38 +96,32 @@ void solve() {
 
     //achar o primeiro menor valor andando a partir do 0
     cur = 1;
-    while (true && cur <= 2*n){
+    while (cur <= 2*n){
         if (tele[cur].porzero) {
-            if (c >= tele[cur].value) {
-                c -= tele[cur].value;
-                ans++;
-                cur++;
-                tele.erase(tele.begin() + cur);
-            }
+            cout << "achei" << endl;
             break;
         }
+        cur++;
     }
 
-    while (tele.begin() != tele.end() && c >= tele[1].value) {
+    if (c >= tele[cur].value) {
+        c -= tele[cur].value;
+        ans++;
+        //vamos deletar o objeto encontrado e seu simétrico
+        remover_simetricos(tele, tele[cur], cur, n);
+
+    } else {
+        c = -1;
+    }
+
+    //loop que vai pegar o menor valor disponível, e realizar o teletransporte naquela casa
+    while (tele.size() > 1 && c >= tele[1].value) {
         //tira o valor do teletransporte e incrementa a resposta
         c -= tele[1].value;
         ans++;
-        //value that needs to be removed
-        int valueRemove;
-        int indexRemove = tele[cur].index;
-        if (tele[cur].porzero) {
-            valueRemove = tele[cur].value + n - 2 * cur + 1;
-        }
-        else {
-            valueRemove = tele[cur].value + 2 * cur - n - 1;
-        }
-        //apaga o teletransporte que acabamos de usar
-        tele.erase(tele.begin() + 1);
-
-        //binary search to find the value to remove
-
+        remover_simetricos(tele, tele[1], 1, n);
     }
-
+    cout << ans << endl;
 }
  
 int32_t main() {
